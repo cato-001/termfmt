60f6676dc7b2f75e585ca8ca27a94aad60a50136
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-04 18:32:39 +0200

    add stderr to output

--- src/output.rs
+++ src/output.rs
@@ -2,15 +2,100 @@ use std::fmt::{self, Display, Formatter};
 use std::io::{stderr, stdout, IsTerminal};
 
 pub trait TermOutput {
-    fn stdout(
+    fn termout(
         self,
         interactive: impl Fn(Self, &mut Formatter) -> fmt::Result,
         unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result,
     ) -> Self;
 
-    fn stdout_interactive(self, interactive: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self;
+    fn termout_interactive(self, interactive: impl Fn(Self, &mut Formatter) -> fmt::Result)
+        -> Self;
 
-    fn stdout_unstyled(self, unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self;
+    fn termout_unstyled(self, unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self;
+
+    fn termerr(
+        self,
+        interactive: impl Fn(Self, &mut Formatter) -> fmt::Result,
+        unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result,
+    ) -> Self;
+
+    fn termerr_interactive(self, interactive: impl Fn(Self, &mut Formatter) -> fmt::Result)
+        -> Self;
+
+    fn termerr_unstyled(self, unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self;
+}
+
+impl<Value> TermOutput for Value
+where
+    Value: Copy,
+{
+    fn termout(
+        self,
+        interactive: impl Fn(Self, &mut Formatter) -> fmt::Result,
+        unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result,
+    ) -> Self {
+        if is_stdout_interactive() {
+            println!("{}", FnFmt::new(interactive, self));
+        } else {
+            println!("{}", FnFmt::new(unstyled, self));
+        }
+        self
+    }
+
+    fn termout_interactive(
+        self,
+        interactive: impl Fn(Self, &mut Formatter) -> fmt::Result,
+    ) -> Self {
+        if is_stdout_interactive() {
+            println!("{}", FnFmt::new(interactive, self));
+        }
+        self
+    }
+
+    fn termout_unstyled(self, unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self {
+        if !is_stdout_interactive() {
+            println!("{}", FnFmt::new(unstyled, self));
+        }
+        self
+    }
+
+    fn termerr(
+        self,
+        interactive: impl Fn(Self, &mut Formatter) -> fmt::Result,
+        unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result,
+    ) -> Self {
+        if is_stderr_interactive() {
+            eprintln!("{}", FnFmt::new(interactive, self));
+        } else {
+            eprintln!("{}", FnFmt::new(unstyled, self));
+        }
+        self
+    }
+
+    fn termerr_interactive(
+        self,
+        interactive: impl Fn(Self, &mut Formatter) -> fmt::Result,
+    ) -> Self {
+        if is_stderr_interactive() {
+            eprintln!("{}", FnFmt::new(interactive, self));
+        }
+        self
+    }
+
+    fn termerr_unstyled(self, unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self {
+        if !is_stderr_interactive() {
+            eprintln!("{}", FnFmt::new(unstyled, self));
+        }
+        self
+    }
+}
+
+fn is_stdout_interactive() -> bool {
+    stdout().is_terminal()
+}
+
+fn is_stderr_interactive() -> bool {
+    stderr().is_terminal()
 }
 
 struct FnFmt<Function, Value> {
@@ -36,44 +121,4 @@ where
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         (self.function)(self.value, f)
     }
-}
-
-impl<Value> TermOutput for Value
-where
-    Value: Copy,
-{
-    fn stdout(
-        self,
-        interactive: impl Fn(Self, &mut Formatter) -> fmt::Result,
-        unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result,
-    ) -> Self {
-        if is_stdout_interactive() {
-            println!("{}", FnFmt::new(interactive, self));
-        } else {
-            println!("{}", FnFmt::new(unstyled, self));
-        }
-        self
-    }
-
-    fn stdout_interactive(self, interactive: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self {
-        if is_stdout_interactive() {
-            println!("{}", FnFmt::new(interactive, self));
-        }
-        self
-    }
-
-    fn stdout_unstyled(self, unstyled: impl Fn(Self, &mut Formatter) -> fmt::Result) -> Self {
-        if !is_stdout_interactive() {
-            println!("{}", FnFmt::new(unstyled, self));
-        }
-        self
-    }
-}
-
-fn is_stdout_interactive() -> bool {
-    stdout().is_terminal()
-}
-
-fn is_stderr_interactive() -> bool {
-    stderr().is_terminal()
 }
