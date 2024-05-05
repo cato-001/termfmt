d27176645d0e5b0d27ba8177b255b3bcf4c7d217
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-05 10:12:13 +0200

    update output trait

--- src/output.rs
+++ src/output.rs
@@ -1,52 +1,33 @@
-use std::fmt::Display;
 use std::io::{stderr, stdout, IsTerminal};
 
 pub trait TermOutput {
-    fn termout<InteractiveResult, UnstyledResult>(
+    fn termout(
         self,
-        interactive: impl Fn(&Self) -> InteractiveResult,
-        unstyled: impl Fn(&Self) -> UnstyledResult,
-    ) -> Self
-    where
-        InteractiveResult: Display,
-        UnstyledResult: Display;
+        interactive: impl Fn(&Self) -> String,
+        unstyled: impl Fn(&Self) -> String,
+    ) -> Self;
 
-    fn termout_interactive<Result>(self, interactive: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display;
+    fn termout_interactive(self, interactive: impl Fn(&Self) -> String) -> Self;
 
-    fn termout_unstyled<Result>(self, unstyled: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display;
+    fn termout_unstyled(self, unstyled: impl Fn(&Self) -> String) -> Self;
 
-    fn termerr<InteractiveResult, UnstyledResult>(
+    fn termerr(
         self,
-        interactive: impl Fn(&Self) -> InteractiveResult,
-        unstyled: impl Fn(&Self) -> UnstyledResult,
-    ) -> Self
-    where
-        InteractiveResult: Display,
-        UnstyledResult: Display;
+        interactive: impl Fn(&Self) -> String,
+        unstyled: impl Fn(&Self) -> String,
+    ) -> Self;
 
-    fn termerr_interactive<Result>(self, interactive: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display;
+    fn termerr_interactive(self, interactive: impl Fn(&Self) -> String) -> Self;
 
-    fn termerr_unstyled<Result>(self, unstyled: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display;
+    fn termerr_unstyled(self, unstyled: impl Fn(&Self) -> String) -> Self;
 }
 
 impl<Value> TermOutput for Value {
-    fn termout<InteractiveResult, UnstyledResult>(
+    fn termout(
         self,
-        interactive: impl Fn(&Self) -> InteractiveResult,
-        unstyled: impl Fn(&Self) -> UnstyledResult,
-    ) -> Self
-    where
-        InteractiveResult: Display,
-        UnstyledResult: Display,
-    {
+        interactive: impl Fn(&Self) -> String,
+        unstyled: impl Fn(&Self) -> String,
+    ) -> Self {
         if is_stdout_interactive() {
             println!("{}", interactive(&self));
         } else {
@@ -55,35 +36,25 @@ impl<Value> TermOutput for Value {
         self
     }
 
-    fn termout_interactive<Result>(self, interactive: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display,
-    {
+    fn termout_interactive(self, interactive: impl Fn(&Self) -> String) -> Self {
         if is_stdout_interactive() {
             println!("{}", interactive(&self));
         }
         self
     }
 
-    fn termout_unstyled<Result>(self, unstyled: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display,
-    {
+    fn termout_unstyled(self, unstyled: impl Fn(&Self) -> String) -> Self {
         if !is_stdout_interactive() {
             println!("{}", unstyled(&self));
         }
         self
     }
 
-    fn termerr<InteractiveResult, UnstyledResult>(
+    fn termerr(
         self,
-        interactive: impl Fn(&Self) -> InteractiveResult,
-        unstyled: impl Fn(&Self) -> UnstyledResult,
-    ) -> Self
-    where
-        InteractiveResult: Display,
-        UnstyledResult: Display,
-    {
+        interactive: impl Fn(&Self) -> String,
+        unstyled: impl Fn(&Self) -> String,
+    ) -> Self {
         if is_stderr_interactive() {
             eprintln!("{}", interactive(&self));
         } else {
@@ -92,20 +63,14 @@ impl<Value> TermOutput for Value {
         self
     }
 
-    fn termerr_interactive<Result>(self, interactive: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display,
-    {
+    fn termerr_interactive(self, interactive: impl Fn(&Self) -> String) -> Self {
         if is_stderr_interactive() {
             eprintln!("{}", interactive(&self));
         }
         self
     }
 
-    fn termerr_unstyled<Result>(self, unstyled: impl Fn(&Self) -> Result) -> Self
-    where
-        Result: Display,
-    {
+    fn termerr_unstyled(self, unstyled: impl Fn(&Self) -> String) -> Self {
         if !is_stderr_interactive() {
             eprintln!("{}", unstyled(&self));
         }
