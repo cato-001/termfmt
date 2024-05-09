5d6996f420bbe56a5c3d3f858fc3493fefecc624
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-09 17:20:25 +0200

    remove chrono as feature

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,13 @@ pub use {
     output::{termarrow, termerr, terminfo, termout, TermError, TermOutput},
 };
 
-#[cfg(feature = "chrono")]
-pub mod chrono;
-
 #[cfg(feature = "command")]
 pub mod command;
 
 #[cfg(feature = "layout")]
 pub mod layout;
 
+pub mod chrono;
 pub mod strategies;
 
 mod color;
